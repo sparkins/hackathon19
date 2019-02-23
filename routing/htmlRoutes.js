67378f1a3da4e952f926fb4ca0bf11ac4ed2a5b3
Sparky <17860180+sparkins@users.noreply.github.com>
2019-02-23 14:29:49 -0800

    created first round for the home page

---
+++ routing/htmlRoutes.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+var path = require('path');
+
+var mongojs = require('mongojs')
+
+app.use(express.static("public"));
+
+module.exports = function (app) {
+
+    app.get('/home', function (req, res) {
+        if (req.session.user_id && req.cookies.user_sid) {
+          res.render(path.join(__dirname, '/../public/index.html'))
+        }
+        else {
+          res.render("home")
+        }
+      });
+
+}

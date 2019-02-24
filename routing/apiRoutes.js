828c03a0c42e0054c4c3f855cedb1c7af4109b98
Sparky <17860180+sparkins@users.noreply.github.com>
2019-02-23 18:05:16 -0800

    new apiroute to get all scores for a particular game

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,26 +1,42 @@
-// var mongoose = require("mongoose");
+var mongoose = require("mongoose");
+var mongojs = require("mongojs");
 
-// mongoose.connect("mongodb://localhost/Scores", { useNewUrlParser: true });
 
-module.exports = function (app) {}
+// Require all models
+var db = require("../models");
 
-//     db.Scores.create({ playerName: "Simon", gameID: 1, Score: 1650 })
-//         .then(function (dbScore) {
-//             // If saved successfully, print the new Library document to the console
-//             console.log(dbScore);
-//         })
-//         .catch(function (err) {
-//             // If an error occurs, print it to the console
-//             console.log(err.message);
-//         });
+mongoose.connect("mongodb://localhost/ScoresDB", { useNewUrlParser: true });
 
-//     app.get('/all-scores', function (req, res) {
-//         db.Scores.find({})
-//             .then(function (dbScore) {
-//                 res.json(dbScore);
-//             })
-//             .catch(function (err) {
-//                 res.json(err);
-//             });
-//     });
-// }
+module.exports = function (app) {
+
+    // db.Scores.create({ playerName: "Simon", gameID: 1, Score: 1650 })
+    // 	.then(function (dbScore) {
+    // 		// If saved successfully, print the new Library document to the console
+    // 		console.log(dbScore);
+    // 	})
+    // 	.catch(function (err) {
+    // 		// If an error occurs, print it to the console
+    // 		console.log(err.message);
+    // 	});
+
+    app.get('/all-scores', function (req, res) {
+        db.Scores.find({})
+            .then(function (dbScore) {
+                res.json(dbScore);
+            })
+            .catch(function (err) {
+                res.json(err);
+            });
+    });
+
+    app.get("/game/:id", function (req, res) {
+        db.Scores.find({
+            "gameID": (req.params.id)
+          }).then(function (dbScores) {
+            res.json(dbScores)
+          })
+            .catch(function (err) {
+              res.json(err)
+            })
+        });
+}
--- server.js
+++ server.js
@@ -2,45 +2,14 @@ var express = require('express');
 
 var app = express();
 
-var logger = require("morgan");
-var mongoose = require("mongoose");
-
 var PORT = 3000;
 
-// Require all models
-var db = require("./models");
-
 app.use(express.static("public"));
-
-var bodyParser = require('body-parser')
 
 var PORT = process.env.PORT || 3000;
 
 require('./routing/apiRoutes')(app);
 require('./routing/htmlRoutes')(app);
-
-mongoose.connect("mongodb://localhost/ScoresDB", { useNewUrlParser: true });
-
-// db.Scores.create({ playerName: "Simon", gameID: 1, Score: 1650 })
-// 	.then(function (dbScore) {
-// 		// If saved successfully, print the new Library document to the console
-// 		console.log(dbScore);
-// 	})
-// 	.catch(function (err) {
-// 		// If an error occurs, print it to the console
-// 		console.log(err.message);
-// 	});
-
-
-app.get('/all-scores', function (req, res) {
-	db.Scores.find({})
-		.then(function (dbScore) {
-			res.json(dbScore);
-		})
-		.catch(function (err) {
-			res.json(err);
-		});
-});
 
 app.listen(PORT, function () {
 	console.log('listening on ' + PORT);
